255e6cd61816b8a964b4cf53de95a1c76ca52f56
konthis <kdkonstanti@csd.auth.gr>
2026-06-20 21:51:07 +0300

    init setup on objects and physics

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,85 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include "colors.hpp"
+#include <glm/trigonometric.hpp>
+
 #include <iostream>
 
-int main(int argc, char **argv){
-    std::cout << "?\n";
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
+void mouse_button_callback(GLFWwindow* window, int button, int action, int mods);
+void error_callback(int error, const char* description);
+
+const GLuint WIDTH = 800, HEIGHT = 600;
+
+int main()
+{
+    // Init GLFW
+    glfwInit();
+
+    glfwSetErrorCallback(error_callback);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+
+    GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "PongGL", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        exit(EXIT_FAILURE);
+    }
+    glfwMakeContextCurrent(window);
+
+    // Set the required callback functions
+    glfwSetKeyCallback(window, key_callback);
+    glfwSetMouseButtonCallback(window, mouse_button_callback);
+
+    int version = gladLoadGL();
+    if (version == 0)
+    {
+        std::cout << "Failed to initialize OpenGL context" << std::endl;
+        return -1;
+    }
+
+    // Define the viewport dimensions
+    glViewport(0, 0, WIDTH, HEIGHT);
+
+    // Game loop
+    while (!glfwWindowShouldClose(window))
+    {
+        // Check if any events have been activated (key pressed, mouse moved etc.) and call corresponding response functions
+        glfwPollEvents();
+
+        glClearColor(0.1f,0.1f,0.1f,1.0f);
+        // glClearColor(0.5f, 0.5f, 0.5f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+    }
+
+    // Terminates GLFW, clearing any resources allocated by GLFW.
+    glfwTerminate();
     return 0;
+}
+
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode)
+{
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, GL_TRUE);
+}
+
+void mouse_button_callback(GLFWwindow* window, int button, int action, int mods)
+{
+    if(button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS) 
+    {
+       double xpos, ypos;
+       glfwGetCursorPos(window, &xpos, &ypos);
+       std::cout << "Cursor Position at (" << xpos << " : " << ypos << ")\n";
+    }
+}
+
+void error_callback(int error, const char* description)
+{
+    fprintf(stderr, "Error: %s\n", description);
 }
---
+++ src/physics.cpp
@@ -0,0 +1,11 @@
+#include "physics.hpp"
+
+
+
+void apply_forces(const GameObject &object, 
+                std::vector<glm::vec3> &forces){
+    
+    for(glm::vec3 f:forces){
+    }
+    
+}
